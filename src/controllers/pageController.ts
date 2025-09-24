310a46f7c29cb4a521b7822c14554eeab635783a
Jonatha Santos <jonathasantos.dev@hotmail.com>
2025-09-24 13:59:59 -0300

    deixando o site responsivo

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,39 @@
 import { Request, Response } from "express";
+import {createMenuobject} from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuobject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (req: Request, res: Response) => {
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuobject('dog'),
+        banner: {
+            title: 'Chachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 export const cats = (req: Request, res: Response) => {
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuobject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 export const fishes = (req: Request, res: Response) => {
-    //res.render('pages/page);
+    res.render('pages/page', {
+        menu: createMenuobject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
